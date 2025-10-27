298a37bd04e84533dd3bd2c0cab7d009e44b377a
belen3434 <ev.parra@duocuc.cl>
2025-10-26 21:23:28 -0300

    Se configura el android manifest y se corrigen errores

--- app/src/main/java/com/example/appmilsaboreskotlin/MainActivity.kt
+++ app/src/main/java/com/example/appmilsaboreskotlin/MainActivity.kt
@@ -1,3 +1,4 @@
+package com.example.appmilsaboreskotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +14,7 @@ import com.example.appmilsaboreskotlin.ui.theme.AppMilSaboresKotlinTheme
 import com.example.appmilsaboreskotlin.ui.view.*
 import com.example.appmilsaboreskotlin.ui.viewmodel.UsuarioViewModel
 import com.example.appmilsaboreskotlin.ui.viewmodel.UsuarioViewModelFactory
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
